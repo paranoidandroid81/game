4c876c60ba2f1f64ae15e325a825e2d1c362453f
Mikey Korst <mpk44@pitt.edu>
2020-02-19 19:43:02 +0000

    Finished guessing game

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,42 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+const secretNum = getRandomInt(1, 9);
+const squares = document.querySelectorAll('.square') as NodeListOf<HTMLDivElement>;
+
+let currSquare = 1;
+squares.forEach(sq => {
+    if (currSquare === secretNum) {
+        sq.dataset.secret = 'true';
+    }
+    currSquare++;
+    sq.addEventListener('click', handleClick);
+});
+
+export function getRandomInt(min: number, max: number) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function handleClick() {
+    const that = this as HTMLDivElement;
+    console.log('Got clicked', that);
+
+    if (that.dataset.secret) {
+        that.classList.add('winner');
+        that.removeEventListener('click', handleClick);
+        squares.forEach(sq => {
+            if (sq !== that) {
+                sq.classList.add('loser');
+            }
+        });
+    } else {
+        that.classList.add('loser');
+        that.removeEventListener('click', handleClick);
+    }
+}
+
+function genRandInRangeOneToSix() {
+
+}
